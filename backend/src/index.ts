79c60ce7f1233f1caad9b4cf24d21ad80b43de3c
engy <esadik@student.ubc.ca>
2025-11-03 14:44:24 -0800

    issue with exit code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,7 +42,7 @@ process.on('unhandledRejection', (reason: unknown) => {
   // Gracefully close server and exit
   server.close(() => {
     logger.info('Server closed due to unhandled rejection');
-    process.exitCode = 1;
+    process.exit(1);
   });
 });
 
@@ -51,6 +51,6 @@ process.on('uncaughtException', (error: Error) => {
   // Gracefully close server and exit
   server.close(() => {
     logger.info('Server closed due to uncaught exception');
-    process.exitCode = 1;
+    process.exit(1);
   });
 });
